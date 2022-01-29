e7cc98f23b5977d0eb5e87a16b41ec0c2979e8f8
Krystian Krawiec <kkicur1@tlen.pl>
2022-01-29 21:02:07 +0100

    6.2. Czym jest Test Driven Development (TDD)? Zadanie: pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,41 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator(7,3);
+        int sum = calculator.add();
+        int substraction = calculator.substract();
+
+
+        if (sum==10) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (substraction==4) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+
+
+
     }
 }
